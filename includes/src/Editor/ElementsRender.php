98fe3a558f1c542d595b30ad72c774b2a2516349
Thiago Senna <trsenna@gmail.com>
2020-04-10 17:16:56 -0300

    added elements render

--- includes/src/Editor/ElementsRender.php
+++ includes/src/Editor/ElementsRender.php
@@ -11,8 +11,26 @@ class ElementsRender
 {
     public function __invoke( $block, $content = '', $isPreview = false, $postId = 0 )
     {
-        print vsprintf( "<div style='color: #999; border: 1px solid #999; padding: 15px'>%s</div>", [
-            __( 'Reorder, reuse and combine elements to build unique content.', 'miralca' ),
-        ] );
+
+        if ( !have_rows( 'chosen_elements' ) ) {
+
+            print vsprintf( "<div style='color: #999; border: 1px solid #999; padding: 15px'>%s</div>", [
+                __( 'Reorder, reuse and combine elements to build unique content.', 'miralca' ),
+            ] );
+
+        }
+
+        while ( have_rows( 'chosen_elements' ) ) {
+
+            the_row();
+
+            $element = get_sub_field( 'element' );
+
+            if ( is_a( $element, \WP_Post::class ) ) {
+                print strval( $element->post_content );
+            }
+
+        }
+
     }
 }
