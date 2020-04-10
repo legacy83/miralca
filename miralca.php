19c275b8afdb7da7f641388d98a48670e566bda0
Thiago Senna <trsenna@gmail.com>
2020-04-10 12:24:09 -0300

    first commit

---
+++ miralca.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Miralca
+ * Plugin URI:  https://github.com/trsenna/mazoo
+ * Author:      Thiago Senna
+ * Author URI:  http://thremes.com.br
+ * Description: Reorderable, reusable elements that combine to build unique content.
+ * Version:     0.1.0
+ *
+ * @package   Mazoo
+ * @author    Thiago Senna <thiago@thremes.com.br>
+ * @copyright Copyright (c) 2019, Thiago Senna
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
