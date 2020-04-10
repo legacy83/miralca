05a47d3913d553d2bdbc74d99cb7713360dde69a
Thiago Senna <trsenna@gmail.com>
2020-04-10 15:35:15 -0300

    saving progress

--- includes/src/Type/Register.php
+++ includes/src/Type/Register.php
@@ -23,8 +23,14 @@ class Register implements BootstrapInterface, ServiceProviderInterface
         add_action( 'init', function () {
 
             $getPostTypes = new GetPostTypes();
-            
+
             foreach ( $getPostTypes as $postType ) {
+
+                $showInMenu = strval( trim( $postType[ 'show_in_menu_string' ] ) );
+
+                $showInMenu = empty( $showInMenu ) ?
+                    filter_var( $postType[ 'show_in_menu' ], FILTER_VALIDATE_BOOLEAN ) :
+                    $showInMenu;
 
                 register_post_type(
                     $postType[ 'name' ],
@@ -34,6 +40,7 @@ class Register implements BootstrapInterface, ServiceProviderInterface
                         'description' => $postType[ 'description' ],
                         'public' => filter_var( $postType[ 'public' ], FILTER_VALIDATE_BOOLEAN ),
                         'show_in_rest' => filter_var( $postType[ 'show_in_rest' ], FILTER_VALIDATE_BOOLEAN ),
+                        'show_in_menu' => $showInMenu,
                         'menu_icon' => $postType[ 'menu_icon' ],
                         'supports' => $postType[ 'supports' ],
                     ], $postType[ 'name' ] )
