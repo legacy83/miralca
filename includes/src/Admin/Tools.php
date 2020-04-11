3a2e629d8f5ec83c5fd6967f7453685fa84ff8fa
Thiago Senna <trsenna@gmail.com>
2020-04-11 16:47:57 -0300

    refactoring admin/tools done

--- includes/src/Admin/Tools.php
+++ includes/src/Admin/Tools.php
@@ -5,6 +5,7 @@ namespace Miralca\Admin;
 use Dalen\Contracts\BootstrapInterface;
 use Dalen\Contracts\DI\ServiceProviderInterface;
 use Dalen\DI\ServiceProviderTrait;
+use function Miralca\view;
 
 /**
  * Class Tools
@@ -20,25 +21,30 @@ class Tools implements BootstrapInterface, ServiceProviderInterface
      */
     public function __bootstrap(): void
     {
-        add_action( 'admin_menu', function () {
+        add_action( 'admin_menu', [ $this, 'submenuPage' ] );
+        add_action( 'admin_enqueue_scripts', [ $this, 'enqueueScripts' ] );
+    }
 
-            add_submenu_page(
-                'edit.php?post_type=miralca_element',
-                __( 'Tools', 'miralca' ),
-                __( 'Tools', 'miralca' ),
-                'manage_options',
-                'miralca-admin-tools',
-                new ToolsRender(),
-            );
+    public function submenuPage()
+    {
+        add_submenu_page(
+            'edit.php?post_type=miralca_element',
+            __( 'Tools', 'miralca' ),
+            __( 'Tools', 'miralca' ),
+            'manage_options',
+            'miralca-admin-tools',
+            [ $this, 'submenuPageRender' ],
+        );
+    }
 
-        } );
+    public function submenuPageRender()
+    {
+        print view( 'admin/tools' )->render();
+    }
 
-        add_action( 'admin_enqueue_scripts', function () {
-
-            $style = plugins_url( 'resources/assets/css/admin/tools.css', MIRALCA_PLUGIN_FILE );
-            wp_enqueue_style( 'miralca-admin-tools', $style );
-            
-        } );
-
+    public function enqueueScripts()
+    {
+        $style = plugins_url( 'resources/assets/css/admin/tools.css', MIRALCA_PLUGIN_FILE );
+        wp_enqueue_style( 'miralca-admin-tools', $style );
     }
 }
---
+++ resources/views/admin/tools.tpl.php
@@ -0,0 +1,9 @@
+<div class="wrap">
+
+    <?= sprintf( '<h1>%s</h1>', __( 'Tools', 'miralca' ) ); ?>
+
+    <?= sprintf( '<p>%s</p>', __( 'All of the selectable code snippets below are useful if you are wanting to migrate layouts and sections from this site to another.', 'miralca' ) ); ?>
+
+    <?php do_action( 'miralca/admin/tools' ); ?>
+
+</div>
