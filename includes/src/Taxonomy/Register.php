ccffc5e0212dd7da956c0fe9c49f534f2a6ebc7d
Thiago Senna <trsenna@gmail.com>
2020-04-11 17:56:30 -0300

    taxonomy register refactoring done

--- includes/src/Taxonomy/Register.php
+++ includes/src/Taxonomy/Register.php
@@ -5,6 +5,8 @@ namespace Miralca\Taxonomy;
 use Dalen\Contracts\BootstrapInterface;
 use Dalen\Contracts\DI\ServiceProviderInterface;
 use Dalen\DI\ServiceProviderTrait;
+use Miralca\Helper\Collection;
+use Miralca\Local\Taxonomies;
 
 /**
  * Class Register
@@ -20,27 +22,27 @@ class Register implements BootstrapInterface, ServiceProviderInterface
      */
     public function __bootstrap(): void
     {
-        add_action( 'init', function () {
+        add_action( 'init', [ $this, 'registerTaxonomy' ] );
+    }
 
-            $getTaxonomies = new GetTaxonomies();
+    public function registerTaxonomy()
+    {
+        $taxonomies = Collection::wrap( new Taxonomies() );
+        $taxonomies->each( function ( array $taxonomy, string $name ) {
 
-            foreach ( $getTaxonomies as $taxonomy ) {
-
-                register_taxonomy(
-                    $taxonomy[ 'name' ],
-                    $taxonomy[ 'object_types' ],
-                    apply_filters( 'miralca/taxonomy/args', [
-                        'label' => $taxonomy[ 'label' ],
-                        'labels' => apply_filters( 'miralca/taxonomy/labels', $taxonomy[ 'labels' ], $taxonomy[ 'name' ] ),
-                        'public' => filter_var( $taxonomy[ 'public' ], FILTER_VALIDATE_BOOLEAN ),
-                        'hierarchical' => filter_var( $taxonomy[ 'hierarchical' ], FILTER_VALIDATE_BOOLEAN ),
-                        'show_in_menu' => filter_var( $taxonomy[ 'show_in_menu' ], FILTER_VALIDATE_BOOLEAN ),
-                        'show_admin_column' => filter_var( $taxonomy[ 'show_admin_column' ], FILTER_VALIDATE_BOOLEAN ),
-                        'show_in_rest' => filter_var( $taxonomy[ 'show_in_rest' ], FILTER_VALIDATE_BOOLEAN ),
-                    ], $taxonomy[ 'name' ] )
-                );
-
-            }
+            register_taxonomy(
+                sanitize_key( $name ),
+                $taxonomy[ 'object_types' ],
+                apply_filters( 'miralca/taxonomy/args', [
+                    'label' => $taxonomy[ 'label' ],
+                    'labels' => apply_filters( 'miralca/taxonomy/labels', $taxonomy[ 'labels' ], $taxonomy[ 'name' ] ),
+                    'public' => filter_var( $taxonomy[ 'public' ], FILTER_VALIDATE_BOOLEAN ),
+                    'hierarchical' => filter_var( $taxonomy[ 'hierarchical' ], FILTER_VALIDATE_BOOLEAN ),
+                    'show_in_menu' => filter_var( $taxonomy[ 'show_in_menu' ], FILTER_VALIDATE_BOOLEAN ),
+                    'show_admin_column' => filter_var( $taxonomy[ 'show_admin_column' ], FILTER_VALIDATE_BOOLEAN ),
+                    'show_in_rest' => filter_var( $taxonomy[ 'show_in_rest' ], FILTER_VALIDATE_BOOLEAN ),
+                ], $taxonomy[ 'name' ] )
+            );
 
         } );
     }
